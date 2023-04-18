8e9e0d79103ddb30be8fc183142019cc58638f7d
mizti <mizugokoro@gmail.com>
2023-04-18 11:15:30 +0900

    added components

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faFilePen, faArrowRightToBracket, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = ({ isAuth }) => {
+    return (
+        <nav>
+            <Link to="/"><FontAwesomeIcon icon={faHome} />ホーム</Link>
+            <Link to="/createpost"><FontAwesomeIcon icon={faFilePen} />記事投稿</Link>
+            {!isAuth  ?
+            <Link to="/login"><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
+            :
+            <Link to="/logout"><FontAwesomeIcon icon={faArrowLeft} />ログアウト</Link>}
+        </nav>
+    );
+}
+
+export default Navbar
